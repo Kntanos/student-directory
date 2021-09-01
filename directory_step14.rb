79a355add20871e865ab854efe25e7da0728a724
KNtanos <kostasntanos@hotmail.com>
2021-09-01 18:08:23 +0100

    added copy of directory for exercises in step 14

---
+++ directory_step14.rb
@@ -0,0 +1,115 @@
+@students =[] 
+
+def print_menu
+    puts [
+    "1. Input the students",
+    "2. Show the students",
+    "3. Save the list to students.csv",
+    "4. Load the list of students.csv",
+    "9. Exit"
+]
+end
+
+def interactive_menu
+    loop do
+        print_menu
+        process(STDIN.gets.chomp)
+    end
+end
+
+def process(selection)
+    case selection
+    when "1"
+        input_students
+    when "2"
+        show_students
+    when "3"
+        save_students
+    when "4"
+        load_students
+    when "9"
+        exit
+    else
+        puts "I don't know what you meant, try again"
+    end
+end
+
+def input_students
+    puts "Please enter the names of the students"
+    puts "To finish, just hit return twice"
+    @name = STDIN.gets.chomp
+
+    while !@name.empty? do
+        add_to_list()
+        puts "Now we have" + " #{@students.count}#{@students.count > 1 ? " students" : " student"}"
+        @name = STDIN.gets.chomp
+    end
+    @students
+end
+
+def add_to_list(cohort = "November")
+    @students << {name: @name, cohort: cohort.to_sym}
+end
+
+def show_students
+    print_header
+    print_student_list
+    print_footer
+end
+
+def print_header
+    puts "The students of Villains Academy"
+    puts "-------------"
+end
+
+def print_student_list
+    exit if @students.count == 0
+    @students.each do |student|
+        puts "#{student[:name]} (#{student[:cohort]} cohort)"
+    end
+end
+
+def print_footer
+    if @students.count > 1
+        noun = "students"
+    else noun = "student"
+    end
+    puts "Overall, we have #{@students.count} great #{noun}"
+end
+
+def save_students
+    file = File.open("students.csv", "w")
+    @students.each do |student|
+        student_data = [student[:name], student[:cohort]]
+        csv_line = student_data.join(",")
+        file.puts csv_line
+    end
+    file.close
+end
+
+def load_students(filename = "students.csv")
+    file = File.open(filename, "r")
+    file.readlines.each do |line|
+    @name, cohort = line.chomp.split(",")
+        add_to_list(cohort)
+    end
+    file.close
+end
+
+def try_load_students
+    filename = ARGV.first 
+    return if filename.nil? 
+    if File.exists?(filename)
+        load_students(filename)
+          puts "Loaded #{@students.count} from #{filename}"
+    else 
+        puts "Sorry, #{filename} doesn't exists."
+        exit 
+    end
+end
+
+try_load_students
+interactive_menu
+
+
+
