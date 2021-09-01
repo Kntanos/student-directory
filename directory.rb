89698ac1646c9b0c3fd1f1d585ee4d6f5c142b60
KNtanos <kostasntanos@hotmail.com>
2021-09-01 14:05:07 +0100

    step 12 done

--- directory.rb
+++ directory.rb
@@ -1,4 +1,13 @@
-@students =[] # create an empty array
+def load_students
+    file = File.open("students.csv", "r")
+    file.readlines.each do |line|
+    name, cohort = line.chomp.split(",") #WOW!!! Paraller assignemnt of two variables from the split in one go!
+        @students << {name: name, cohort: cohort.to_sym}
+    end
+    file.close
+end
+
+#@students =[] # create an empty array
 
 def interactive_menu
     loop do
@@ -13,6 +22,10 @@ def process(selection)
         input_students
     when "2"
         show_students
+    when "3"
+        save_students
+    when "4"
+        load_students
     when "9"
         exit # this will cause the program to terminate
     else
@@ -23,6 +36,8 @@ end
 def print_menu
     puts "1. Input the students"
     puts "2. Show the students"
+    puts "3. Save the list to students.csv"
+    puts "4. Load the list of students.csv"
     puts "9. Exit"
 end
 
@@ -46,8 +61,7 @@ def input_students
         # get another name from the user
         name = gets.chomp
     end
-    # return the array of students
-    @students
+    @students # return the array of students
 end
 
 
@@ -71,5 +85,19 @@ def print_footer
     puts "Overall, we have #{@students.count} great #{x}"
 end
 
+def save_students
+    # open a file for writing
+    file = File.open("students.csv", "w")
+    # iterate over the array of students
+    @students.each do |student|
+        student_data = [student[:name], student[:cohort]]
+        csv_line = student_data.join(",")
+        file.puts csv_line
+    end
+    file.close
+end
+
 interactive_menu
-#students = input_students
+
+
+
